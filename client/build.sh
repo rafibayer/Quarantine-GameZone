2601fd7109df55fe99f416e8a13515c67a9c5e7a
Vivian Hua <viviancarolinehua@gmail.com>
2020-05-29 10:37:59 -0700

    remove dockerfile for now

--- client/build.sh
+++
@@ -1,1 +0,0 @@
-docker build -t viviancarolinehua/gamezone_client .
--- client/deploy.sh
+++
@@ -1,7 +0,0 @@
-docker rm -f gamezone_client
-
-docker run  \
--d \
--p 3000:80 \
---name gamezone_client \
-viviancarolinehua/gamezone_client
